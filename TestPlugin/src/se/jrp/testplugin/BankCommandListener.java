190d29718c8b0f8185c4244b4e06ccc5310b0d8d
John Rapp Farnes <johnfarnes@hotmail.com>
2013-11-10 02:53:18 +0100

    added BankInventory.java

--- TestPlugin/src/se/jrp/testplugin/BankCommandListener.java
+++
@@ -1,7 +0,0 @@
-package se.jrp.testplugin;
-
-import org.bukkit.entity.Player;
-
-public interface BankCommandListener {
-	void onCommand(Player player, String[] args);
-}
--- TestPlugin/src/se/jrp/testplugin/Resources/Integers.java
+++
@@ -1,5 +0,0 @@
-package se.jrp.testplugin.Resources;
-
-public class Integers {
-	public final static int BANK_MAX_SLOTS = 54;
-}
