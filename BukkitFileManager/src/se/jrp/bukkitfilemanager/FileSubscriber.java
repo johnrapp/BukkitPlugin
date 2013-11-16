0c281aadfae02fe67582b00701ca829769dbbf45
John Rapp Farnes <johnfarnes@hotmail.com>
2013-11-16 20:30:46 +0100

    Netbeans conversion

---
+++ BukkitFileManager/src/se/jrp/bukkitfilemanager/FileSubscriber.java
@@ -0,0 +1,10 @@
+package se.jrp.bukkitfilemanager;
+
+
+
+import java.util.HashMap;
+
+public interface FileSubscriber {
+	void onLoad(String id, HashMap<? extends Object, ? extends Object> map);
+	HashMap<? extends Object, ? extends Object> onSave(String id);
+}
--- TestPlugin/dist/start.bat
+++
@@ -1,2 +0,0 @@
-java -Xmx1024M -jar craftbukkit.jar -o true
-PAUSE
